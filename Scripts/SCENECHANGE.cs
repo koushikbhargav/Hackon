32435acb6e595c235e1a2d068e62decf0f5d6368
EpperlaKarthik <52861755+EpperlaKarthik@users.noreply.github.com>
2020-04-18 21:31:57 +0530

    Add files via upload

---
+++ Scripts/SCENECHANGE.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SCENECHANGE : MonoBehaviour
+{
+   public void firstpagetologinpage()
+    {
+        SceneManager.LoadScene("Loginpage");
+    }
+    public void loginpagetohomepage()
+    {
+        SceneManager.LoadScene("homepage");
+    }
+    public void homepagetoselection()
+    {
+        SceneManager.LoadScene("selection");
+    }
+    public void selectiontosingleplayer()
+    {
+        SceneManager.LoadScene("singleplayer");
+    }
+    public void selectiontomultiplayer()
+    {
+        SceneManager.LoadScene("multiplayer");
+    }
+    public void singleplayertoselection()
+    {
+        SceneManager.LoadScene("selection");
+    }
+    public void multiplayertoselection()
+    {
+        SceneManager.LoadScene("selection");
+    }
+    public void singletovr()
+    {
+        SceneManager.LoadScene("vrheadset1");
+    }
+    public void multitovr()
+    {
+        SceneManager.LoadScene("vrheadset2");
+    }
+    public void loadvrwarningtomulti()
+    {
+        SceneManager.LoadScene("multiplayer");
+    }
+    public void loadvrwarningtosingle()
+    {
+        SceneManager.LoadScene("singleplayer");
+    }
+
+}
+
+
